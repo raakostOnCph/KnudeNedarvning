12e162d6e505d0a0d10c97e7e804baf977590e91
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2019-04-08 20:04:27 +0200

    getInstans er en tosset maade at goere det paa men her kan du se det.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,40 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+//
+//        Liste liste = new Liste();
+//
+//        liste.insertFromHead(new NodeEng("John"));
+//        liste.insertFromHead(new NodeDk());
+//        liste.insertFromHead(new NodeDk("søren"));
+//        liste.insertFromHead(new NodeEng("Victoria"));
+//        liste.insertFromHead(new NodeEng());
+//
+//        liste.udskriv();
 
-        Liste liste = new Liste();
+        NodeEng nodeEng = new NodeEng("læskdjf");
 
-        liste.insertFromHead(new NodeEng("John"));
-        liste.insertFromHead(new NodeDk());
-        liste.insertFromHead(new NodeDk("søren"));
-        liste.insertFromHead(new NodeEng("Victoria"));
-        liste.insertFromHead(new NodeEng());
-
-        liste.udskriv();
+        System.out.println(getInfor(nodeEng));
+        System.out.println(getInfor(new NodeDk("palle")));
+        System.out.println(getInfor(new NodeEng("henning")));
+        System.out.println(getInfor(new NodeDk("ole") ));
 
 
+    }
+
+    // her ligger logikken centralt i stedet for i de enkelte knudetyper
+    public static String getInfor(NodeAbs nodeAbs) {
+
+        if (nodeAbs instanceof NodeDk) {
+            return "dansk";
+        }
+
+        if (nodeAbs instanceof NodeEng) {
+            return "english";
+        }
+
+
+        return null;
     }
 }
